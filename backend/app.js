f428530402bb8c3a59ea89617423e862ac80ff7d
GabContreras <contrerasgab01@gmail.com>
2025-05-15 23:47:32 -0600

    Creación de proyectos frontend y backend, instalación de dependencias

---
+++ backend/app.js
@@ -0,0 +1,21 @@
+//funciones de la app, tipo login, crud, registros, etc
+
+//inserto todo lo de la libreria express
+import express from "express";
+import cookieParser from "cookie-parser";
+//import employeesRoutes from "./routes/employees.js";
+
+// Creo una constante que es igual a la libreria que acabo de importar, y la ejecuto 
+
+const app = express();
+
+//Middleware para que acepte datos JSON
+app.use(express.json());
+//Que acepte cookies en postman
+app.use(cookieParser());
+
+//app.use("/api/employees/", employeesRoutes);
+
+
+//Exporto esta constante para usar express en todos lados
+export default app;
