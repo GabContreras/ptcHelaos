61643e7af72150fd76ee33a8484e0fec15281cb4
SrAdrielM <20230542@ricaldone.edu.sv>
2025-07-11 22:59:49 -0600

    haciendo seguimiento de orden y calificacion de servicio

--- backend/src/routes/logoutRoutes.js
+++
@@ -1,8 +0,0 @@
-import express from 'express'
-import logout from '../controllers/logoutController.js'
-
-const router = express.Router()
-
-router.route('/').post(logout.logout)
-
-export default router
