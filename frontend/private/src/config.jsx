0edb744211a484c1bb5dbfdfecf92ea285b0f6ea
GabContreras <contrerasgab01@gmail.com>
2025-08-19 10:33:45 -0600

    url

--- frontend/private/src/config.jsx
+++ frontend/private/src/config.jsx
@@ -1,6 +1,6 @@
 export const config = {
     api: {
-        API_BASE: "https://ptchelaos.onrender.com/api/docs/api/"
+        API_BASE: "https://ptchelaos.onrender.com/api/"
         //API_BASE: import.meta.env.VITE_API_BASE
     },
 
