b34b7493d79d78e38782ccd7f455455ed3a0f43f
GabContreras <contrerasgab01@gmail.com>
2025-08-18 23:53:45 -0600

    url de render

--- frontend/public/src/config.jsx
+++ frontend/public/src/config.jsx
@@ -1,10 +1,6 @@
 export const config = {
     api: {
-<<<<<<< HEAD
-        API_BASE: "http://ptchelaos.onrender.com/api/"
-=======
-        API_BASE: "http://localhost:4000/api/"
->>>>>>> Dixi
+        API_BASE: "https://ptchelaos.onrender.com/api/"
         //API_BASE: import.meta.env.VITE_API_BASE
     },
 
